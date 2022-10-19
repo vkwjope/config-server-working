fbcdf0cabfd542f5c9b8bb1a602d8c467c300258
Vinod Kumar <58595080+vkwjope@users.noreply.github.com>
2022-10-19 15:07:07 +0530

    Add files via upload

---
+++ employee-config-server/src/main/java/com/example/employeeconfigserver/EmployeeConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.employeeconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class EmployeeConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeConfigServerApplication.class, args);
+	}
+
+}
